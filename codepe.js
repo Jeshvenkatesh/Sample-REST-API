c6f3646e443d9e4cc2a58e17df0ec7fcdc3618a8
Jeshvenkatesh <Jeshvenkatesh@users.noreply.github.com>
2020-09-24 16:08:54 +0530

    Initial commit

---
+++ codepe.js
@@ -0,0 +1,32 @@
+// need to be write it on code pen
+
+
+// const getButton = document.getElementById('get');
+// const postButton = document.getElementById('Posts');
+
+// getButton.addEventListener("click", () => {
+//   fetch("http://localhost:8080/feed/posts")
+//   .then(res => res.json())
+//   .then(resData => console.log(resData))
+//   .catch(err => console.log(err))
+// });
+
+// postButton.addEventListener("click", ()=> {
+//   fetch("http://localhost:8080/feed/post",{
+//     method: 'POST',
+//     body: JSON.stringify({
+//       title : 'A codepen post',
+//       content : 'created via codepen'
+//     }),
+//     headers: {
+//       'Content-Type' : 'application/json'
+//     }
+//   })
+//   .then(res => res.json())
+//   .then(resData => console.log(resData))
+//   .catch(err => console.log(err))
+// });
+
+
+// <button id="get">GET Posts</button>
+// <button id="Posts">Create a Post</button>
